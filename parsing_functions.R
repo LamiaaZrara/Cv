bc5a5cd25a062141fac482efc0e1d15c9716a8af
LamiaaZrara <88975192+LamiaaZrara@users.noreply.github.com>
2022-10-02 15:50:33 +0100

    This is my CV repo
    This is my CV repo

--- parsing_functions.R
+++ parsing_functions.R
@@ -94,7 +94,7 @@ print_section <- function(position_data, section_id){
 
 # Construct a bar chart of skills
 build_skill_bars <- function(skills, out_of = 5){
-  bar_color <- "#969696"
+  bar_color <- "#08306b"
   bar_background <- "#d9d9d9"
   skills %>% 
     mutate(width_percent = round(100*level/out_of)) %>% 
